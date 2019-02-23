dacfcdd0279ff41ebf7fc1249c51f8d4ac5fe0d6
NoaJunod <noa.junod@googlemail.com>
2019-02-23 16:41:44 +0100

    added sudoku creation (doesn't work yet)

--- src/main/java/Creator.java
+++ src/main/java/Creator.java
@@ -3,20 +3,34 @@ import java.util.Random;
 public class Creator {
 
     private int[][] sudoku;
+    private int n, sqrtN;
 
-    public Creator () {
-        sudoku = new int[9][9];
+    public Creator(int n, int sqrtN) {
+        this.n = n;
+        this.sqrtN = sqrtN;
+        sudoku = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sudoku[i][j] = 0;
+            }
+        }
+        createSudoku();
+
+    }
+
+    public void createSudoku(){
         fillDiagonalFields();
+        fillInAll();
     }
 
     public void fillDiagonalFields() {
-        int[][] small = new int[3][3];
-        Random r = new Random();
         int nextNumber;
-        for (int i = 0; i < 3; i++) {
-            for (int x = 0; x < 3; x++) {
-                for (int y = 0; y < 3; y++) {
-                    nextNumber = r.nextInt(9) + 1;
+        for (int i = 0; i < sqrtN; i++) {
+            int[][] small = new int[sqrtN][sqrtN];
+            Random r = new Random();
+            for (int x = 0; x < sqrtN; x++) {
+                for (int y = 0; y < sqrtN; y++) {
+                    nextNumber = r.nextInt(n) + 1;
                     if (checkIfAlreadyUsed(nextNumber, small)) {
                         if (y == 0) {
                             x--;
@@ -28,42 +42,188 @@ public class Creator {
                     }
                 }
             }
+            int counter = 0;
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    counter++;
+                    sudoku[x + (i * 3)][y + (i * 3)] = small[x][y];
+                    System.out.println(sudoku[x + (i * 3)][y + (i * 3)]);
+                }
+            }
+
             String out = "";
             for (int a = 0; a < 3; a++) {
-                for (int j = 0; j < 3; j++) {
-                    out += small[a][j] + " ";
+                for (int b = 0; b < 3; b++) {
+                    out += small[a][b] + " ";
                 }
                 out += "\n";
             }
             System.out.println(out);
-            if(i == 0){
-                for(int x = 0; x < 3; x++){
-                    for(int y = 0; y < 3; y++){
-                        small[x][y] = sudoku[x][y];
+
+        }
+        String out = "";
+        for (int a = 0; a < 9; a++) {
+            for (int b = 0; b < 9; b++) {
+                out += sudoku[a][b] + " ";
+            }
+            out += "\n";
+        }
+        System.out.println(out);
+
+    }
+
+    public void fillInAll() {
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                if (sudoku[x][y] == 0) {
+                    Random r = new Random();
+                    int number = r.nextInt(9) + 1;
+                    for (int i = 1; i <= 9; i++) {
+                        if (checkIfSafe(x, y, i)) {
+                            sudoku[x][y] = i;
+                        }
+                        if(i == 9) {
+                            if(y == 0) {
+                                x--;
+                            } else {
+                                y--;
+                            }
+                            for (int j = 0; j < 9; j++) {
+                                sudoku[j][y] = 0;
+                            }
+                        }
                     }
                 }
-            } else if(i == 1){
-                for(int x = 0; x < 3; x++){
-                    for(int y = 0; y < 3; y++){
-                        small[x][y] = sudoku[x + 3][y + 3];
-                    }
-                }
-            } else if(i == 2){
-                for(int x = 0; x < 3; x++){
-                    for(int y = 0; y < 3; y++){
-                        small[x][y] = sudoku[x + 6][y + 6];
-                    }
+            }
+        }
+        String out = "";
+        for (int a = 0; a < 9; a++) {
+            for (int b = 0; b < 9; b++) {
+                out += sudoku[a][b] + " ";
+            }
+            out += "\n";
+        }
+        System.out.println(out);
+    }
+
+    public boolean fillRemaining(int i, int j) {
+        //  System.out.println(i+" "+j);
+        if (j >= n && i < n - 1) {
+            i = i + 1;
+            j = 0;
+        }
+        if (i >= n && j >= n) {
+            return true;
+        }
+
+        if (i < sqrtN) {
+            if (j < sqrtN) {
+                j = sqrtN;
+            }
+        } else if (i < n - sqrtN) {
+            if (j == (i / sqrtN) * sqrtN) {
+                j = j + sqrtN;
+            }
+        } else {
+            if (j == n - sqrtN) {
+                i = i + 1;
+                j = 0;
+                if (i >= n) {
+                    return true;
                 }
             }
         }
 
 
+        for (int num = 1; num <= n; num++) {
+            if (checkIfSafe(i, j, num)) {
+                sudoku[i][j] = num;
+                if (fillRemaining(i, j + 1)) {
+                    return true;
+                }
+                sudoku[i][j] = 0;
+            }
+        }
+
+        String out = "";
+        for (int a = 0; a < 9; a++) {
+            for (int b = 0; b < 9; b++) {
+                out += sudoku[a][b] + " ";
+            }
+            out += "\n";
+        }
+        System.out.println(out);
+
+        return false;
     }
 
-    public boolean checkIfAlreadyUsed(int number, int[][] matrix){
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(number == matrix[i][j]){
+    public boolean checkIfHasHole() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (sudoku[i][j] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkIfSafe(int i, int j, int num) {
+        if(unUsedInRow(i, num) &&
+                unUsedInCol(j, num) &&
+                unUsedInBox(i - i % sqrtN, j - j % sqrtN, num)){
+            return true;
+        }
+        return false;
+    }
+
+    // check in the row for existence
+    public boolean unUsedInRow(int i, int num) {
+        for (int j = 0; j < n; j++) {
+            if (sudoku[i][j] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // check in the row for existence
+    public boolean unUsedInCol(int j, int num) {
+        for (int i = 0; i < n; i++) {
+            if (sudoku[i][j] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Returns false if given 3 x 3 block contains num.
+    boolean unUsedInBox(int rowStart, int colStart, int num) {
+        for (int i = 0; i < sqrtN; i++)
+            for (int j = 0; j < sqrtN; j++)
+                if (sudoku[rowStart + i][colStart + j] == num)
+                    return false;
+
+        return true;
+    }
+
+    public boolean checkIfAlreadyUsed(int number, int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (number == matrix[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkIfAlreadyUsedInRowOrColumn(int number, int[][] matrix, int row, int column) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[column][j] == number) {
+                    return true;
+                } else if (matrix[i][row] == number) {
                     return true;
                 }
             }
