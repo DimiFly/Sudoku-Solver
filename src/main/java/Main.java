5604e571039bfa737d4e53955e70a33ac99763fa
DimiFly <dimitri.h.wehrli@gmail.com>
2019-03-29 13:23:35 +0100

    connection to controller, mainclass update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(new URL("file:src/main/resources/SudokuGenerator.fxml"));
+        primaryStage.setTitle("Voronoi");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        Creator c = new Creator(9, 3, 17);
-        Solver s = new Solver(c.getSudoku(), 9, 3);
+       launch(args);
     }
 }
--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -1,6 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+
 public class ViewController {
 
     private Creator creator;
     private Solver solver;
+    private GraphicsContext gc;
+
+    @FXML
+    private Canvas canvas;
+
+
 
 }
