656b9a4ffba89a506663d924d7074626af1e237a
NoaJunod <noa.junod@googlemail.com>
2019-03-29 13:06:22 +0100

    updated stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Creator c = new Creator(9, 3, 17);
-        Solver s = new Solver(c.getSudoku(), 9, 3);
+        Creator c = new Creator(9, 3, 41);
+        Solver s = new Solver(c.getSudoku(), c.getN(), c.getSqrtN());
     }
 }
--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -1,6 +1,12 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
 public class ViewController {
 
     private Creator creator;
     private Solver solver;
+
+    @FXML
+    private Canvas canvas;
 
 }
