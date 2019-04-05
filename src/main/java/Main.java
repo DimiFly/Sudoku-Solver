7cf9f33e528fb83dac56da901b301ebddc3d1799
DimiFly <dimitri.h.wehrli@gmail.com>
2019-04-05 13:26:50 +0200

    :bug:gui changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -16,6 +17,7 @@ public class Main extends Application {
         primaryStage.setTitle("Sudoku");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
